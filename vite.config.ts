b3a70603d4df0d0ced92ded7351f07c7bc44b627
Saidi <139466730+saidinyanguila@users.noreply.github.com>
2026-02-09 17:51:35 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,21 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	// server: {
+	// 	host: "::",
+	// 	port: 8080,
+	// },
+	// plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+	// resolve: {
+	// 	alias: {
+	// 	"@": path.resolve(__dirname, "./src"),
+	// 	},
+	// },
+	base: '/',
+	  plugins: [react()],
+	  resolve: {
+		alias: {
+		  '@': path.resolve(__dirname, './src'),
+		},
+	  },
 }));
